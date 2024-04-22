3f2d405d582e1a77ac48cfa37345087433ca0bc0
Sunav Sapkota <sunavsapkota57@gmail.com>
2024-04-22 15:12:24 +0545

    improving

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Front from '../Components/Front/Front'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
   return (
 <>
     <Front/>
+    <Popular/>
+    <Offers/>
 </>
   )
 }
